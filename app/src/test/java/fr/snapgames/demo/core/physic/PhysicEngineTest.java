93df09f0b6d35ae06d78af213599b0694ed9c01c
Frédéric Delorme <frederic.delorme@gmail.com>
2023-01-16 11:05:24 +0100

    bugfix(): Unit Tests on PhysicEngine and Renderer were down.
    - update the test configuration files and fix some bugs in RendererTest and PhysicEngineTest.

--- app/src/test/java/fr/snapgames/demo/core/physic/PhysicEngineTest.java
+++ app/src/test/java/fr/snapgames/demo/core/physic/PhysicEngineTest.java
@@ -1,6 +1,7 @@
 package fr.snapgames.demo.core.physic;
 
 import fr.snapgames.demo.core.entity.Entity;
+import fr.snapgames.demo.core.entity.GameObject;
 import fr.snapgames.demo.gdemoapp.App;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -55,29 +56,29 @@ public class PhysicEngineTest {
         // create some test entities
         for (int i = 0; i < 10; i++) {
             game.getEntityManager().add(
-                    new Entity("TestEntity_" + i)
+                    new GameObject("TestEntity_" + i)
                             .setSize(16.0, 16.0)
                             .setPosition(0.0, 0.0)
                             .setSpeed(20.0, 20.0)
                             .setMass(1.0)
                             .setMaterial(Material.RUBBER)
                             .addForce(new Point2D.Double(
-                                    (Math.random() * 2.0) - 1.0,
-                                    (Math.random() * 2.0) - 1.0)));
+                                    (Math.random() * 102.0) - 1.0,
+                                    (Math.random() * 102.0) - 1.0)));
         }
         // update the engine
         for (int i = 0; i < 10; i++) {
-            pe.update(16);
+            pe.update(1600);
         }
         // check entities position
         for (int i = 0; i < 10; i++) {
-            Entity e = game.getEntityManager().get("TestEntity_" + i);
-            System.out.printf("- entity %s pos:%3.0f,%3.0f spd:%3.2f,%3.3f, acc: %3.2f,%3.3f%n",
+            Entity<?> e = game.getEntityManager().get("TestEntity_" + i);
+            System.out.printf("- Entity[name:%s;pos:%3.0f,%3.0f;spd:%3.2f,%3.2f;acc: %3.2f,%3.2f]%n",
                     e.getName(),
                     e.x, e.y,
                     e.dx, e.dy,
                     e.ax, e.ay);
-            Assertions.assertTrue(e.y != 0.0 && e.y != 0.0, "Entity " + i + " has not been updated: x=" + e.x + ",y=" + e.y);
+            Assertions.assertTrue(e.x != 0.0 || e.y != 0.0, "Entity " + i + " has not been updated: x=" + e.x + ",y=" + e.y);
         }
     }
 }
